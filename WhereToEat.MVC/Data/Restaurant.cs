b5e66770b8951e16db78a81875512b668934e2d6
abramepithey <abramepithey@gmail.com>
2021-06-11 16:38:55 -0600

    Reconfigures Restaurant data model to use more simple price field

--- WhereToEat.MVC/Data/Restaurant.cs
+++ WhereToEat.MVC/Data/Restaurant.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace WhereToEat.MVC.Data
 {
@@ -6,14 +7,14 @@ namespace WhereToEat.MVC.Data
     {
         public Guid RestaurantId { get; set; }
         public string Name { get; set; }
+        
+        [Range(1, 5, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public int Price { get; set; }
 
         public Guid StyleId { get; set; }
         public Style Style { get; set; }
         
         public Guid CuisineId { get; set; }
         public Cuisine Cuisine { get; set; }
-
-        public Guid PriceId { get; set; }
-        public Price Price { get; set; }
     }
 }
