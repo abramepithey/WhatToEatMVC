9cb4b58e7625ac43e43b081ad0070d14910e7c39
abramepithey <abramepithey@gmail.com>
2021-06-13 22:09:04 -0600

    Adds interfaces for services classes of Cuisines and Styles

---
+++ WhereToEat.MVC/Contracts/ICuisineServices.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WhereToEat.MVC.Models.Cuisines;
+
+namespace WhereToEat.MVC.Contracts
+{
+    public interface ICuisineServices
+    {
+        Task<IList<CuisineViewModel>> ListCuisines();
+        Task<bool> CreateCuisine(CuisineViewModel newCuisine);
+        Task<CuisineViewModel> GetCuisineById(Guid cuisineId);
+        Task<bool> UpdateCuisine(Guid cuisineId, CuisineDetailsModel updatedCuisine);
+        Task<bool> DeleteCuisine(Guid cuisineId);
+    }
+}
---
+++ WhereToEat.MVC/Contracts/IStyleServices.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WhereToEat.MVC.Models.Styles;
+
+namespace WhereToEat.MVC.Contracts
+{
+    public interface IStyleServices
+    {
+        Task<IList<StyleViewModel>> ListCuisines();
+        Task<bool> CreateCuisine(StyleViewModel newCuisine);
+        Task<StyleViewModel> GetCuisineById(Guid cuisineId);
+        Task<bool> UpdateCuisine(Guid cuisineId, StyleDetailsModel updatedCuisine);
+        Task<bool> DeleteCuisine(Guid cuisineId);
+    }
+}
