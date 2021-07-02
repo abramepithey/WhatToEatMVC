24f9451dcadb9619af45c0cbebbb27f39fb69c5d
abramepithey <abramepithey@gmail.com>
2021-07-01 22:06:39 -0600

    Adds navigation properties to the new entities in the ApplicationUser data model

--- WhereToEat.MVC/Data/ApplicationUser.cs
+++ WhereToEat.MVC/Data/ApplicationUser.cs
@@ -6,5 +6,6 @@ namespace WhereToEat.MVC.Data
     public class ApplicationUser : IdentityUser
     {
         public IEnumerable<Connection> Connections { get; set; }
+        public IEnumerable<ChoiceMember> ChoiceMemberships { get; set; }
     }
 }
